4081152db64da76bfbeb2550f39fd9b39cbc1435
Chris Tonkinson <chris@tonkinson.com>
2026-03-31 08:13:26 -0400

    Refactor for a single source of truth

--- Formula/factestio.rb
+++ Formula/factestio.rb
@@ -12,21 +12,12 @@ class Factestio < Formula
   # Lua 5.2 and luarocks are required (not available in Homebrew core).
   # Install via luaver: https://github.com/DhavalKapil/luaver
   #   luaver install 5.2.4 && luaver use 5.2.4
-  #   luarocks install --deps-only factestio-0.1-0.rockspec
+  #   luarocks install --deps-only factestio-*.rockspec
   depends_on "git"
 
   def install
     libexec.install Dir["*"]
-
-    # Write a wrapper that hardcodes FACTESTIO_ROOT to libexec.
-    # Assumes lua (5.2.x via luaver) and required rocks are on PATH.
-    (bin/"factestio").write <<~SH
-      #!/usr/bin/env zsh
-      export FACTESTIO_ROOT="#{libexec}"
-      export LUA_PATH="#{libexec}/?.lua;#{libexec}/?/init.lua;./?.lua;./?/init.lua;$LUA_PATH"
-      exec lua "#{libexec}/run.lua" "$@"
-    SH
-    (bin/"factestio").chmod 0755
+    bin.install_symlink libexec/"bin/factestio"
   end
 
   test do
@@ -39,7 +30,7 @@ class Factestio < Formula
       If you haven't already, install them:
 
         luaver install 5.2.4 && luaver use 5.2.4
-        luarocks install --deps-only #{opt_libexec}/factestio-0.1-0.rockspec
+        luarocks install --deps-only #{opt_libexec}/factestio-*.rockspec
 
       See https://github.com/DhavalKapil/luaver for luaver installation.
     EOS
