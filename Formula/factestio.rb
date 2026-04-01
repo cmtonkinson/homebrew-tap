ad5e47e9961c2e97dfa61ea10a1e4741203326d1
Chris Tonkinson <chris@tonkinson.com>
2026-04-01 07:59:21 -0400

    factestio v0.2.3

--- Formula/factestio.rb
+++ Formula/factestio.rb
@@ -2,30 +2,30 @@
 # frozen_string_literal: true
 
 class Factestio < Formula
-  desc "Hierarchical scenario-based test framework for Factorio mods"
-  homepage "https://github.com/cmtonkinson/factestio"
-  license "MIT"
-  url "https://github.com/cmtonkinson/factestio/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "7d251e6bd2c7a1f5e6adfcf54d78c92da2e64c0665c2a4a8af19f512496d6556"
-  version "0.2.2"
+  desc 'Hierarchical scenario-based test framework for Factorio mods'
+  homepage 'https://github.com/cmtonkinson/factestio'
+  license 'MIT'
+  url 'https://github.com/cmtonkinson/factestio/archive/refs/tags/v0.2.3.tar.gz'
+  sha256 'a5c9c2669c97cb6a2af6892a0a44a4dc0c533a0f6970e586d2a092e542655215'
+  version '0.2.3'
 
   # Lua 5.2 and luarocks are required (not available in Homebrew core).
   # Install via luaver: https://github.com/DhavalKapil/luaver
   #   luaver install 5.2.4 && luaver use 5.2.4
   #   luarocks install --deps-only factestio-*.rockspec
-  depends_on "git"
+  depends_on 'git'
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/factestio"
+    libexec.install Dir['*']
+    bin.install_symlink libexec / 'bin/factestio'
   end
 
   test do
-    system "#{bin}/factestio", "--help"
+    system "#{bin}/factestio", '--help'
   end
 
   def caveats
-    <<~EOS
+    <<~CAVEATS
       factestio requires Lua 5.2 and LuaRocks dependencies.
       If you haven't already, install them:
 
@@ -33,6 +33,6 @@ class Factestio < Formula
         luarocks install --deps-only #{opt_libexec}/factestio-*.rockspec
 
       See https://github.com/DhavalKapil/luaver for luaver installation.
-    EOS
+    CAVEATS
   end
 end
